7f99f85906480440130d7468945bfcc58b0e1fff
Kamil Król <kamil.krol.1997@gmail.com>
2020-12-02 16:45:51 +0100

    🎄

---
+++ Day2/Day2.py
@@ -0,0 +1,23 @@
+#%%
+#!===PART 1===!#
+input = open("input.txt", "r")
+total = 0
+for line in input:
+    line = line.split()
+    low, high = map(int,line[0].split('-'))
+    sign = line[1][0]
+    password = line[2]
+    if low <= password.count(sign) <= high: total+=1
+print(total)
+#%%
+#!===PART 2===!#
+input = open("input.txt", "r")
+total = 0
+for line in input:
+    line = line.split()
+    pos1, pos2 = map(int,line[0].split('-'))
+    sign = line[1][0]
+    password = line[2]
+    if (password[pos1-1] == sign) != (password[pos2-1] == sign):total+=1
+print(total)
+# %%
